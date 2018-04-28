ce07467fb751850f19cc623bd4d655d737b939b1
vishwanath <vishwanath.pratap91@gmail.com>
2018-04-28 23:46:29 +0530

    first ArrayList collection

---
+++ src/com/fc/coll/List.java
@@ -0,0 +1,13 @@
+package com.fc.coll;
+
+public interface List <E> {
+	
+	public void add(E e);
+	
+	public E get(int index);
+	
+	public Object remove(int index);
+	
+	public void display();
+
+}
---
+++ src/com/fc/coll/Test.java
@@ -0,0 +1,12 @@
+package com.fc.coll;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		list.add("AAA");
+		list.add("bbb");
+		
+		list.display();
+	}
+}
